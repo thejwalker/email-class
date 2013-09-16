2c2b4fdef896a3e29260454275cb809e4518d669
Jon Walker <jon@aristoworks.com>
2013-09-16 12:23:08 -0500

    Added GOV and MIL Functions

--- email-class.php
+++ email-class.php
@@ -29,9 +29,35 @@
 	 		} 
 	 	}
 
-
+	 	/**
+	 	 * Checks whether or not a given email address is for an educational institutioin
+	 	 * @param  string $email The Email Address
+	 	 * @return bool        IS EDU / IS NOT EDU
+	 	 */
 	 	function if_edu($email) {
 	 		if (preg_match('/^[a-z0-9_\-]+(\.[_a-z0-9\-]+)*@([_a-z0-9\-]+\.)+edu$/i', $email)) {
+	 			return TRUE;
+	 		}
+	 	}
+
+	 	/**
+	 	 * Checks whether or not a given email address is for a government entity
+	 	 * @param  string $email The Email Address
+	 	 * @return bool        IS GOV / IS NOT GOV
+	 	 */
+	 	function if_gov($email) {
+	 		if (preg_match('/^[a-z0-9_\-]+(\.[_a-z0-9\-]+)*@([_a-z0-9\-]+\.)+gov$/i', $email)) {
+	 			return TRUE;
+	 		}
+	 	}
+
+	 	/**
+	 	 * Checks whether or not a given email address is from a military entity
+	 	 * @param  string $mail The Email Address
+	 	 * @return bool       IS MIL / IS NOT MIL
+	 	 */
+	 	function if_mil($mail) {
+	 		if (preg_match('/^[a-z0-9_\-]+(\.[_a-z0-9\-]+)*@([_a-z0-9\-]+\.)+mil$/i', $email)) {
 	 			return TRUE;
 	 		}
 	 	}
