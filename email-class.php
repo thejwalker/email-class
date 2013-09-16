1ed19a3c30cf3adac5206fdd35888dd2e0b4ad74
Jon Walker <jon@aristoworks.com>
2013-09-15 19:52:48 -0500

    Added Regex Validation

--- email-class.php
+++ email-class.php
@@ -4,15 +4,17 @@
 	 	var $email = '';
 	 	var $error = '';
 
-	 	//
-	 	// VALIDATE USING REGULAR EXPRESSIONS
-	 	// 
+	 	/**
+	 	 * [validate_reg description]
+	 	 * @param  string $email The email addres you want to validate
+	 	 * @return bool 		 
+	 	 */
 	 	function validate_reg($email) {
 	 		$valid = true;
 	        if (!eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,8})$", $this->email))
 	        {
 	            $valid = false;
-	            $this->error_html = "$this->email is not properly formatted";
+	            $this->error = "$this->email is not properly formatted";
 	        }
 	        return $valid;
 	 	}
